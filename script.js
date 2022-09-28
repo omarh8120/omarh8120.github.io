f20b35651c38f9d70f5f01dd462979de1acc2008
omarh8120 <114589797+omarh8120@users.noreply.github.com>
2022-09-28 09:30:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "OmarH"; // update the part BETWEEN the "quotes"
+let userName = "omarh8120"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
